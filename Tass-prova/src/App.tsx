e467ac79c74e696d98f39eaf9bacff94a7f79072
AldoRambaudo <73881388+AldoRambaudo@users.noreply.github.com>
2023-12-10 20:15:44 +0100

    Taass React Basi
    Ho messo le prime basi per il Menù dell'App

--- Tass-prova/src/App.tsx
+++ Tass-prova/src/App.tsx
@@ -1,12 +1,18 @@
 import viteLogo from '/vite.svg';
 import User from './components/User';
+import Menu from './components/Menu';
+import View from './components/View';
 
 function App() {
-    document.body.style = 'background: #232526;';
+    document.body.style = 'background: #232526; position:relative';
     return (
-        <User></User>
+        <div>
+        <User />
+        <Menu />
+        <View />
+        </div>
 
- 
+
     );
 }
 
---
+++ Tass-prova/src/components/View.tsx
@@ -0,0 +1,11 @@
+function View(){
+    return (<div style={{
+        backgroundColor: '#404040',
+        width: '70%',
+        height:'95vh',
+        position: 'absolute',
+        top: '5vh',
+        left: '45vh',
+        borderRadius: '5%'}}>Ciao</div>)
+}
+export default View;
