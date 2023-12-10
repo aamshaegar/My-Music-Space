e440a1319378f3083551ee13a1d98007e0ef4f48
AldoRambaudo <73881388+AldoRambaudo@users.noreply.github.com>
2023-12-10 17:50:02 +0100

    Test React

---
+++ Tass-prova/src/App.tsx
@@ -0,0 +1,13 @@
+import viteLogo from '/vite.svg';
+import User from './components/User';
+
+function App() {
+    document.body.style = 'background: #232526;';
+    return (
+        <User></User>
+
+ 
+    );
+}
+
+export default App;
---
+++ Tass-prova/src/components/User.tsx
@@ -0,0 +1,8 @@
+function User() {
+    return <h1 style={{
+        backgroundColor: '#3E99CF',
+        width: '20%',
+        height: '20%'
+    }}>User</h1>
+}
+export default User;
