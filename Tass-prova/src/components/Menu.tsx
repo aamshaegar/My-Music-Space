be87c8c58b5f579fce064a6c03f2684dbb4f6668
“Lorenzo <lorenzo.falchi@edu.unito.it>
2023-12-12 16:54:30 +0100

    active e hover sui bottoni

--- Tass-prova/src/components/Menu.tsx
+++ Tass-prova/src/components/Menu.tsx
@@ -3,6 +3,18 @@ const imageUrl2 =
   "https://as1.ftcdn.net/v2/jpg/04/54/29/98/1000_F_454299832_EAOJOc0GKq8S5dsXuv6B79bbIc0GhQmy.jpg";
 
 function Menu() {
+  function selected(id: string) {
+    const elements = Array.from(
+      document.getElementsByClassName(
+        "menuButton"
+      ) as HTMLCollectionOf<HTMLElement>
+    );
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].style.backgroundColor = "#232425";
+    }
+    document.getElementById(id)!.style.backgroundColor = "#3E99CF";
+  }
+
   return (
     <div className="Menu">
       <div>
@@ -15,10 +27,34 @@ function Menu() {
           <img className="immagini" src={imageUrl2} alt="Immagine" />
         </div>
       </div>
-      <button className="musicButton">Home</button>
-      <button className="musicButton">Musica</button>
-      <button className="musicButton">Shop</button>
-      <button className="musicButton">Chat</button>
+      <button
+        className="menuButton"
+        id="homeButton"
+        onClick={() => selected("homeButton")}
+      >
+        Home
+      </button>
+      <button
+        className="menuButton"
+        id="menuButton"
+        onClick={() => selected("menuButton")}
+      >
+        Musica
+      </button>
+      <button
+        className="menuButton"
+        id="shopButton"
+        onClick={() => selected("shopButton")}
+      >
+        Shop
+      </button>
+      <button
+        className="menuButton"
+        id="chatButton"
+        onClick={() => selected("chatButton")}
+      >
+        Chat
+      </button>
     </div>
   );
 }
