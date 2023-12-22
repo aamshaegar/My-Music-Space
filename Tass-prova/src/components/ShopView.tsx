fcf443c553565c274d3c68648e9440aa0a5c328d
AldoRambaudo <73881388+AldoRambaudo@users.noreply.github.com>
2023-12-22 18:54:45 +0100

    Shop View quasi completo
    Bisogna mettere i nomi delle sezioni ancora ma animazioni fatte

--- Tass-prova/src/components/ShopView.tsx
+++ Tass-prova/src/components/ShopView.tsx
@@ -1,5 +1,7 @@
 const ImgVinyl = "/src/img/Vinyl_icon.png"
 const ImgAlbum = "/src/img/Viva la vida.jpg"
+const ImgTicket = "/src/img/elodie-show-2023.jpg"
+const ImgMerch ="/src/img/Cappello PTN.jpg"
 
 function VinylProduct(){
     return(
@@ -11,6 +13,25 @@ function VinylProduct(){
     );
 }
 
+function TicketProduct(){
+    return(
+        <div className="TicketProduct">
+            <img src={ImgTicket} className="TicketFront"></img>
+            <img src={ImgTicket} className="TicketBack"></img>
+            <p>Elodie 2023</p>
+        </div>
+    );
+}
+
+function MerchProduct(){
+    return(
+        <div className="MerchProduct">
+            <img src={ImgMerch} className="Merch"></img>
+            <p>Cappello PTN</p>
+        </div>
+    )
+}
+
 function ShopView() {
     return (
         <div className="ShopView" id = "ShopView">
@@ -20,6 +41,20 @@ function ShopView() {
                 <VinylProduct></VinylProduct>
                 <VinylProduct></VinylProduct>
                 <VinylProduct></VinylProduct>
+            </div>
+            <div className="TicketRow">
+                <TicketProduct></TicketProduct>
+                <TicketProduct></TicketProduct>
+                <TicketProduct></TicketProduct>
+                <TicketProduct></TicketProduct>
+                <TicketProduct></TicketProduct>
+            </div>
+            <div className="MerchRow">
+                <MerchProduct></MerchProduct>
+                <MerchProduct></MerchProduct>
+                <MerchProduct></MerchProduct>
+                <MerchProduct></MerchProduct>
+                <MerchProduct></MerchProduct>
             </div>
         </div>
     );
