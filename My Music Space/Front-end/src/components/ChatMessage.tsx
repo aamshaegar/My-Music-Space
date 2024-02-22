b6949f84529398846d5422accbe0768757cc900c
AldoRambaudo <73881388+AldoRambaudo@users.noreply.github.com>
2024-02-22 10:18:43 +0100

    Piccola modifica e inizio messaggistica

--- My Music Space/Front-end/src/components/ChatMessage.tsx
+++ My Music Space/Front-end/src/components/ChatMessage.tsx
@@ -1,5 +1,16 @@
 import {useEffect} from "react";
 
+const ChatMessages = ["#Pop","#Classic","#Metal","#Funky","#R&B","#Indie","#Soul","Banana","Casa"];
+
+function ChatMex({Message}){
+    return(
+        <div className="ChatMex" id="ChatMex">
+            {console.log("Nuovo messaggio ricevuto:", Message)}
+            <p>{Message}</p>
+        </div>
+    );
+}
+
 function ChatMessage({message}){
     useEffect(() => {
         console.log("Nuovo messaggio ricevuto:", message);
@@ -8,6 +19,11 @@ function ChatMessage({message}){
     return(
     <div className="ChatMessage" id="ChatMessage">
             <p>{message}</p>
+                <div id="Chats">
+                    {ChatMessages.map(Message => (
+                        <ChatMex key={Message} name={Message} />
+                    ))}
+                </div>
     </div>)
 
 }
