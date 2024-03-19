c3cd78f9907c293f4909d32285bbcf5296fa6902
“Lorenzo <lorenzo.falchi@edu.unito.it>
2024-03-19 20:59:19 +0100

    inizio revisione music shop

--- My Music Space/Front-end/src/components/MusicView.tsx
+++ My Music Space/Front-end/src/components/MusicView.tsx
@@ -1,111 +1,131 @@
-import {useState} from "react";
-import {useEffect} from "react";
-const ImgPlaylist ="/src/img/texas-road-trip-playlist.jpg"
-const ImgRadio="/src/img/music-note.png"
-import "../css/MusicView.css"
+import { useState } from "react";
+import { useEffect } from "react";
+const ImgPlaylist = "/src/img/texas-road-trip-playlist.jpg";
+const ImgRadio = "/src/img/music-note.png";
+const imgArtist = "/src/img/harryStyles.webp";
+import "../css/MusicView.css";
 
-function Object({object}){
-    return(
-        <div className="Object">
-            <div className="productImg">
-                <img src={object['imgURL']}></img>
-            </div>
-            <div className="description">{object['title']}</div>
-        </div>
-    );
+function Album({ object }) {
+  return (
+    <div className="albumDiv">
+      <div className="externDiv">
+        <img id="albumImg" src={object["imgURL"]}></img>
+      </div>
+      <div className="description">{object["title"]}</div>
+    </div>
+  );
 }
 
-function MusicView({focus, query}) {
+function Artist({ object }) {
+  return (
+    <div className="artistDiv">
+      <div className="externDiv">
+        <img id="artistImg" src={object["imgURL"]}></img>
+      </div>
+      <div className="description">{object["title"]}</div>
+    </div>
+  );
+}
 
-    // questa lista verrà richiesta quando clicco sul bottone music, quindi sarà memorizzata nel padre e passata al figlio.
-    useEffect(() => {
-        if(focus == "musicButton"){
-            if(query && query != ""){
-                alert("Music view search bar piena OOOOOK")
-            }
-        }
-    },[focus, query]);
+function Song({ object }) {
+  return (
+    <div className="songDiv">
+      <div className="externDiv">
+        <img id="songImg" src={object["imgURL"]}></img>
+      </div>
+      <div className="description">{object["title"]}</div>
+    </div>
+  );
+}
 
-
-    // Type 1 == Album
-    // Type 2 == Artisti
-    // Type 3 == Brani
-
-    const objects = [{
-        title: "Viva la vida",
-        type: 1,
-        imgURL: "/src/img/Viva la vida.jpg",
-    },
-    {
-        title: "Viva la vida",
-        type: 1,
-        imgURL: "/src/img/Viva la vida.jpg",
-    },
-
-    {
-        title: "Viva la vida",
-        type: 1,
-        imgURL: "/src/img/Viva la vida.jpg",
-    },
-
-    {
-        title: "Viva la vida",
-        type: 1,
-        imgURL: "/src/img/Viva la vida.jpg",
-    },
-
-    {
-        title: "Viva la vida",
-        type: 1,
-        imgURL: "/src/img/Viva la vida.jpg",
-    },
-    {
-        title: "Viva la vida",
-        type: 2,
-        imgURL: "/src/img/Viva la vida.jpg",
-    },
-    {
-        title: "Viva la vida",
-        type: 2,
-        imgURL: "/src/img/Viva la vida.jpg",
-    },
-    {
-        title: "Viva la vida",
-        type: 3,
-        imgURL: "/src/img/Viva la vida.jpg",
-    },
-    {
-        title: "Viva la vida",
-        type: 3,
-        imgURL: "/src/img/Viva la vida.jpg",
+function MusicView({ focus, query }) {
+  // questa lista verrà richiesta quando clicco sul bottone music, quindi sarà memorizzata nel padre e passata al figlio.
+  useEffect(() => {
+    if (focus == "musicButton") {
+      if (query && query != "") {
+        alert("Music view search bar piena OOOOOK");
+      }
     }
-]
+  }, [focus, query]);
 
+  // Type 1 == Album
+  // Type 2 == Artisti
+  // Type 3 == Brani
 
-    return (
-        <div className="MusicView" id = "MusicView">
+  const objects = [
+    {
+      title: "Viva la vida",
+      type: 1,
+      imgURL: "/src/img/Viva la vida.jpg",
+    },
+    {
+      title: "Viva la vida",
+      type: 1,
+      imgURL: "/src/img/Viva la vida.jpg",
+    },
 
-            <div className="Container">
-                <div className="title">Artisti</div>
-                    {objects.map((obj,index) => (
-                        obj['type'] == 1 ? (<Object key={index} object={obj} />) : null))
-                    }
-            </div>
-            <div className="Container">
-                <div className="title">Album</div>
-                    {objects.map((obj,index) => (
-                        obj['type'] == 2 ? (<Object key={index} object={obj} />) : null))
-                    }
-            </div>
+    {
+      title: "Viva la vida",
+      type: 1,
+      imgURL: "/src/img/Viva la vida.jpg",
+    },
 
-            <div className="Container">
-                <div className="title">Brani</div>
-                    {objects.map((obj,index) => (
-                        obj['type'] == 3 ? (<Object key={index} object={obj} />) : null))
-                    }
-            </div>
+    {
+      title: "Viva la vida",
+      type: 1,
+      imgURL: "/src/img/Viva la vida.jpg",
+    },
 
-        </div>
-    );
+    {
+      title: "Viva la vida",
+      type: 1,
+      imgURL: "/src/img/Viva la vida.jpg",
+    },
+    {
+      title: "HS",
+      type: 2,
+      imgURL: "/src/img/harryStyles.webp",
+    },
+    {
+      title: "HS",
+      type: 2,
+      imgURL: "/src/img/harryStyles.webp",
+    },
+    {
+      title: "Viva la vida",
+      type: 3,
+      imgURL: "/src/img/Viva la vida.jpg",
+    },
+    {
+      title: "Viva la vida",
+      type: 3,
+      imgURL: "/src/img/Viva la vida.jpg",
+    },
+  ];
+
+  return (
+    <div className="MusicView" id="MusicView">
+      <div className="title">Artisti</div>
+      <div className="Container">
+        {objects.map((obj, index) =>
+          obj["type"] == 1 ? <Album key={index} object={obj} /> : null
+        )}
+      </div>
+
+      <div className="title">Album</div>
+      <div className="Container">
+        {objects.map((obj, index) =>
+          obj["type"] == 2 ? <Artist key={index} object={obj} /> : null
+        )}
+      </div>
+
+      <div className="title">Brani</div>
+      <div className="Container">
+        {objects.map((obj, index) =>
+          obj["type"] == 3 ? <Song key={index} object={obj} /> : null
+        )}
+      </div>
+    </div>
+  );
 }
 export default MusicView;
