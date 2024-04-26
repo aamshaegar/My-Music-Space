550e44d2594a09a8a03664ddb4915c83f317a452
Michele <michele.colombino@edu.unito.it>
2024-04-26 19:04:06 +0200

    Servizio di shop reso raggiungibile dal discovery service

--- My Music Space/Back-end/shop-service/src/test/java/com/example/demo/ChatApplicationTests.java
+++ My Music Space/Back-end/shop-service/src/test/java/com/example/demo/ChatApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.shopservice;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ShopServiceApplicationTests {
+class ChatApplicationTests {
 
     @Test
     void contextLoads() {
